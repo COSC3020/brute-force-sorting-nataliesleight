ab86461120b1da2bb0ad0ae3dcf996b82f511d26
nataliesleight <nsleight@uwyo.edu>
2025-03-05 15:05:25 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function permutationSort(a) {
     if (a.length == 0 || a.length == 1) {
         return 0;
     }
-    var sortValue = [false, -1];
+    var sortValue = [false, 0];
     var tmpArray = new Array(a.length);
     permutations(a, 0, sortValue, tmpArray);
     arr = tmpArray;
