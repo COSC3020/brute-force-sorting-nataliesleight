dda2c0706c16bc25021cac5658a04fc4e704bc14
nataliesleight <nsleight@uwyo.edu>
2025-03-05 15:19:00 -0700

    Update code.js

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ function permutationSort(a) {
         if (a[i] != a[0]) {isHomogeneous = false;}
     }    
     if (isHomogeneous == true) {return 0;}
-    var sortValue = [false, 0]; // including original as permutation, change 0 to -1 to not
+    var sortValue = [false, -1]; // including original as permutation, change 0 to -1 to not
     var tmpArray = new Array(a.length);
     permutations(a, 0, sortValue, tmpArray);
     arr = tmpArray;
